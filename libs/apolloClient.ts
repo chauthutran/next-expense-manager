1d5ac86c16534902ce5532b160cffe91685582b1
chauthutran <cthutran@gmail.com>
2025-09-05 10:33:33 +0900

    initialized the HttpLink with a uri when Apollo Client created

--- libs/apolloClient.ts
+++ libs/apolloClient.ts
@@ -1,4 +1,10 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+// Create a link to your GraphQL server
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL, // <-- REQUIRED
+  credentials: 'same-origin', // optional
+});
 
 const client = new ApolloClient({
     uri: "/api/graphql", // GraphQL API endpoint
